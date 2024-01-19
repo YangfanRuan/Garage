e62c6868bf9f83f2983f8cb2b146eef2a8b4ad70
ruanyangfan <rani20230124@gmail.com>
2024-01-19 18:19:37 +0800

    v0.1 基础版

---
+++ 字符匹配-车牌识别系统/参考代码/office/office/newDlg.cpp
@@ -0,0 +1,34 @@
+﻿// newDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "office.h"
+#include "afxdialogex.h"
+#include "newDlg.h"
+
+
+// newDlg 对话框
+
+IMPLEMENT_DYNAMIC(newDlg, CDialogEx)
+
+newDlg::newDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_OFFICE_DIALOG, pParent)
+{
+
+}
+
+newDlg::~newDlg()
+{
+}
+
+void newDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(newDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// newDlg 消息处理程序
---
+++ 字符匹配-车牌识别系统/参考代码/office/office/newDlg.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// newDlg 对话框
+
+class newDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(newDlg)
+
+public:
+	newDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~newDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_OFFICE_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
